ff15afab83d596a7553b56710d0b20b8948a33dc
Ryota Yamada <roy29fuku@gmail.com>
2017-05-11 16:43:16 +0900

    core

---
+++ djangotest/core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+class Book(models.Model):
+    class Meta:
+        db_table = 'books'
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=datetime.now)
+    def __str__(self):
+        return self.title
+
