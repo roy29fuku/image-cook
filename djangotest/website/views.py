12b81d5125f9726ce5ee51e96ee0347a21d4afb2
Ryota Yamada <roy29fuku@gmail.com>
2017-05-11 17:39:50 +0900

    view books 作成

--- djangotest/website/views.py
+++ djangotest/website/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from core.models import Book
 
 # Create your views here.
 def index(request):
     return render(
         request,
         'website/index.html',
+    )
+
+def books(request):
+    books = Book.objects.all()
+    context = {
+        'books': books,
+    }
+    return render(
+        request,
+        'website/books.html',
+        context,
     )
