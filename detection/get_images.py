3d390bb8d0b34db4fc6818a57fb97169e4841728
Ryota Yamada <roy29fuku@gmail.com>
2017-05-15 16:35:46 +0900

    recipe api課題

---
+++ detection/get_images.py
@@ -0,0 +1,8 @@
+sites = {
+    '1': 'http://photozou.jp/photo/search_result?whose=public&copyright=all&cc_license=cc.ok.ok&keyword=',
+    '2': '',
+    '3': '',
+}
+
+key = input('input keyword: ')
+site = input('choose site (1) フォト蔵, (2) flicker, (3) imagenet: ')
---
+++ kadai/0515_recipe_api.py
@@ -0,0 +1,9 @@
+import requests
+'''
+https://developer.edamam.com/
+'''
+
+url = ''
+ingredient = input('検索したい食材を入力してください: ')
+r = requests.get(url)
+
