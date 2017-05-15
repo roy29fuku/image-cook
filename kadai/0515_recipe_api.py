203d141d8969faaa08cfbb239104de3a941e7293
Ryota Yamada <roy29fuku@gmail.com>
2017-05-15 16:43:02 +0900

    レシピ取得

--- kadai/0515_recipe_api.py
+++ kadai/0515_recipe_api.py
@@ -3,7 +3,10 @@ import requests
 https://developer.edamam.com/
 '''
 
-url = ''
+api_id = 'eb465fbd'
+api_key = 'c750dc940c55d3433b5f33a6f7d3cd2f'
 ingredient = input('検索したい食材を入力してください: ')
-r = requests.get(url)
+url = 'https://api.edamam.com/search?q='+ingredient+'&app_id='+api_id+'&app_key='+api_key
+req = requests.get(url)
+print(req.text)
 
