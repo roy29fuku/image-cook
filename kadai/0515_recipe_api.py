87fdb9cfbdebc244926996fe802b5b304318cde8
Ryota Yamada <roy29fuku@gmail.com>
2017-05-15 16:53:00 +0900

    レシピ整形

--- kadai/0515_recipe_api.py
+++ kadai/0515_recipe_api.py
@@ -5,8 +5,15 @@ https://developer.edamam.com/
 
 api_id = 'eb465fbd'
 api_key = 'c750dc940c55d3433b5f33a6f7d3cd2f'
-ingredient = input('検索したい食材を入力してください: ')
-url = 'https://api.edamam.com/search?q='+ingredient+'&app_id='+api_id+'&app_key='+api_key
-req = requests.get(url)
-print(req.text)
+q = input('検索したい食材を入力してください: ')
+url = 'https://api.edamam.com/search?q='+q+'&app_id='+api_id+'&app_key='+api_key
+response = requests.get(url).json()
+recipe = response['hits'][0]['recipe']
+label = recipe['label']
+ingredients = recipe['ingredients']
+url = recipe['url']
+print('タイトル: ', label)
+for ingredient in ingredients:
+    print('食材: ', ingredient['text'])
+print('URL: ', url)
 
