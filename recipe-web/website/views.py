e2fd7207233abc841e17da065bd5c780e3f34a77
SugiuraShota <s.shota.710.3506@gmail.com>
2017-05-18 17:10:09 +0900

    Update views.py

--- recipe-web/website/views.py
+++ recipe-web/website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from core.models import Book
+import json
 
 # Create your views here.
 def index(request):
@@ -9,11 +10,32 @@ def index(request):
     )
 
 def recipes(request):
-    # context = {
-    #     'books': books,
-    # }
+    jsonScript = '''{
+        "title": "にんじんにんじん",
+        "ingr": [
+            [
+                "にんじん",
+                "1本"
+            ],
+            [
+                "塩",
+                "大さじ1"
+            ],
+            [
+                "砂糖",
+                "大さじ4"
+            ]
+        ],
+        "img": "http://hogehoge.com",
+        "howto": [
+            "にんじんの皮を剥きます",
+            "塩をかけます"
+        ]
+    }'''
+
+    data =json.loads(jsonScript)
     return render(
         request,
         'website/recipes.html',
-        # context,
+        data,
     )
