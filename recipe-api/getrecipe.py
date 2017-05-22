5b5bc3ce91678562f4e66ce367a5c5c804b2ca8e
kurarrr <reverse.engau@gmail.com>
2017-05-23 00:23:48 +0900

    cloring code

--- recipe-api/getrecipe.py
+++ recipe-api/getrecipe.py
@@ -5,11 +5,13 @@ import re
 import pymysql.cursors
 
 def main():
-    url="https://www.kyounoryouri.jp/category/l1"
-    html = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(html, 'lxml')
-    atags=soup.find_all("a")
-    p=re.compile('/recipe/[0-9]{1,5}_*')
+    urls=["https://www.kyounoryouri.jp/search/recipe?keyword=%E3%81%AB%E3%82%93%E3%81%98%E3%82%93&order_type=0",
+         "https://www.kyounoryouri.jp/search/recipe?keyword=%E7%8E%89%E3%81%AD%E3%81%8E",
+         "https://www.kyounoryouri.jp/search/recipe?keyword=%E3%81%8B%E3%81%B6&order_type=0",
+         "https://www.kyounoryouri.jp/search/recipe?keyword=%E3%83%88%E3%83%9E%E3%83%88&order_type=0",
+         "https://www.kyounoryouri.jp/search/recipe?keyword=%E3%82%AD%E3%83%A3%E3%83%99%E3%83%84"]
+    #にんじん、玉ねぎ、かぶ、トマト、キャベツの検索結果
+
 
     connection = pymysql.connect(host='localhost',
                                  user='root',
@@ -24,51 +26,58 @@ def main():
     ingr_ins = "INSERT INTO ingr (recipe_id, name, quantity) VALUES (%s, %s, %s)"
     next_id = 'SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = "kisopro" AND TABLE_NAME = "recipe"'
 
-    for item in atags:
-        link=item.get('href')
-        if link is None:
-            continue
-        if not ('recipe' in link):
-            continue
-        if not p.match(link):
-            continue
-        r=requests.get("https://www.kyounoryouri.jp"+link).text
-        recipe_contents=BeautifulSoup(r,'lxml')
-        title=recipe_contents.find("h1",attrs={"class":"ttl"}).text
-        title=title.strip()
-        imgurl="https://www.kyounoryouri.jp"+recipe_contents.select_one("div.recipe--detail-main img").get("src")
-        ingrs=recipe_contents.select("#ingredients_list > dl")
-        ingr=[]
-        for x in ingrs:
-            a=x.find(class_="ingredient")
-            b=x.find(class_="floatright")
-            if a is None or b is None:
-                continue
-            ingr.append((a.text[1:],b.text))
-        recipe=recipe_contents.select(".howto-sec")
-        howto=[]
-        for x in recipe:
-            a=x.select_one(".howto-sec-val > p")
-            if a is None:
-                continue
-            howto.append(a.text)
-        proc="¥;".join(howto)
+    p = re.compile('/recipe/[0-9]{1,5}_*')
 
-        cursor.execute(next_id)
-        next_recipe_id = cursor[0]["AUTO_INCREMENT"]
+    for url in urls:
+        html = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(html, 'lxml')
+        atags = soup.find_all("a")
 
-        cursor.execute(recipe_ins%(title,imgurl,proc))
-        connection.commit()
-        for ingr_name,ingr_quantity in ingrs:
-            cursor.execute(ingr_ins%(next_recipe_id,ingr_name,ingr_quantity))
+        for item in atags:
+            link=item.get('href')
+            if link is None:
+                continue
+            if not ('recipe' in link):
+                continue
+            if not p.match(link):
+                continue
+            r=requests.get("https://www.kyounoryouri.jp"+link).text
+            recipe_contents=BeautifulSoup(r,'lxml')
+            title=recipe_contents.find("h1",attrs={"class":"ttl"}).text
+            title=title.strip()
+            imgurl="https://www.kyounoryouri.jp"+recipe_contents.select_one("div.recipe--detail-main img").get("src")
+            ingrs=recipe_contents.select("#ingredients_list > dl")
+            ingr=[]
+            for x in ingrs:
+                a=x.find(class_="ingredient")
+                b=x.find(class_="floatright")
+                if a is None or b is None:
+                    continue
+                ingr.append((a.text[1:],b.text))
+            recipe=recipe_contents.select(".howto-sec")
+            howto=[]
+            for x in recipe:
+                a=x.select_one(".howto-sec-val > p")
+                if a is None:
+                    continue
+                howto.append(a.text)
+            proc="¥;".join(howto)
+
+            cursor.execute(next_id)
+            next_recipe_id = cursor.fetchone()["AUTO_INCREMENT"]
+
+            cursor.execute(recipe_ins,(title,imgurl,proc))
             connection.commit()
+            for ingr_name, ingr_quantity in ingr:
+                cursor.execute(ingr_ins,(next_recipe_id,ingr_name,ingr_quantity))
+                connection.commit()
+            '''
+            print("%s %s"%(title,imgurl))
+            for x,y in ingr:
+                print("%s %s"%(x,y))
+            print(proc)
+            '''
 
-        print("%s %s"%(title,imgurl))
-        for x,y in ingr:
-            print("%s %s"%(x,y))
-        print(proc)
-
-        break
 
     cursor.close()
     connection.close()
